f47f87f36a9c743e7c297a5bec69dd22b7bb17a7
rturr <celso.turriago2017@uteq.edu.ec>
2021-12-07 10:53:27 -0500

    Dependencia y creación de la 2da ActivityMain

---
+++ app/src/main/java/com/example/api_restful_turriago/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.api_restful_turriago;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    //  RECORDATORIO PARA CREAR EL TEXT VIEW
+    //  PILAS
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+
+
+}
