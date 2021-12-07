61f4d8ee9c87ca5891f254e43b28afa4b35621db
rturr <celso.turriago2017@uteq.edu.ec>
2021-12-07 13:11:17 -0500

    Lleneado del Request en Volley

--- app/src/main/java/com/example/api_restful_turriago/MainActivity2.java
+++ app/src/main/java/com/example/api_restful_turriago/MainActivity2.java
@@ -3,19 +3,89 @@ package com.example.api_restful_turriago;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity2 extends AppCompatActivity {
 
-
-    //  RECORDATORIO PARA CREAR EL TEXT VIEW
-    //  PILAS
+    TextView textView;
+    RequestQueue requestQueue;
+    private static final String url = "https://gorest.co.in/public/v1/users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        requestQueue = Volley.newRequestQueue(this);
+        initUI();
+        //stringRequest();
+        jsonArrayRequest();
     }
 
+    private void initUI(){
+        textView = findViewById(R.id.txtLista);
+    }
 
+    private void stringRequest(){
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
+                    }
+                }
+        );
+        requestQueue.add(request);
+    }
+
+    private void jsonArrayRequest(){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        int size = response.length();
+                        for (int i=0; i<size; i++){
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.get(i).toString());
+                                String id = jsonObject.getString("id");
+                                textView.append("ID: " + id + "\n");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(jsonArrayRequest);
+    }
 }
